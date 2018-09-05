824e3c4bc3dd717a9c74993b7e6d0f5b9300758d
Kyle Hinckley <kylehinckley@hotmail.com>
2018-09-05 14:22:20 -0600

    Character Controller & Inheritance

---
+++ DGM-2670-001/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMovement : MonoBehaviour {
+
+	public float speed = 6.0F;
+	public float jumpSpeed = 8.0F;
+	public float gravity = 20.0F;
+
+	public FloatData moveX, moveY, MoveZ;
+	
+	private Vector3 moveDirection = Vector3.zero;
+	private CharacterController controller;
+
+	void Start()
+	{
+		controller = GetComponent<CharacterController>();
+	}
+	
+	void Update()
+	{
+
+		
+		if (controller.isGrounded)
+		{
+			//moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			moveDirection.Set(moveX.Value, moveY.Value, MoveZ.Value);
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
+			if (Input.GetButton("Jump"))
+				moveDirection.y = jumpSpeed;
+
+		}
+
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+}
---
+++ DGM-2670-001/Assets/Scripts/FloatData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewFloat", menuName = "KH/FloatData")]
+
+public class FloatData : ScriptableObject
+{
+	public float value;
+	
+	public virtual float Value
+	{
+		get { return value;}
+		//set { Value = value; }
+	}
+	
+}
