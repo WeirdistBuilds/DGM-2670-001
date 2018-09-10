68496e8d5a77837061f5ba053e139a2a956e6f31
Kyle Hinckley <kylehinckley@hotmail.com>
2018-09-10 14:21:49 -0600

    Configurable Character Controller with Rotation

--- DGM-2670-001/Assets/Scripts/FloatInput.cs
+++ DGM-2670-001/Assets/Scripts/FloatInput.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
 
-[CreateAssetMenu(fileName = "FloatInput")]
+[CreateAssetMenu(fileName = "FloatInput", menuName = "KH/FloatData")]
 
 public class FloatInput : FloatData
+//Inherits from FloatData (FloatInput, FloatData, ScriptableObject)
 {
 
 	public string InputType;
 	
+	//Overrides the value of FloatData
 	public override float Value
 	{
-		get { return Input.GetAxis(InputType);}
-		//set { Value = value; }
+		get { return Input.GetAxis(InputType) * value;}
 	}
 }
---
+++ DGM-2670-001/Assets/Scripts/MovePattern.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class MovePattern : ScriptableObject {
+
+	//public float speed = 6.0F;
+	public FloatData JumpSpeed;
+	public FloatData Gravity;
+
+	public FloatData MoveX, MoveY, MoveZ;
+	public FloatData RotX, RotY, RotZ;
+	
+	//vector3s default to zero
+	private Vector3 moveDirection;
+	private Vector3 rotDirection;
+
+	public void Invoke(CharacterController controller, Transform transform)
+	{
+		
+	if (controller.isGrounded)
+	{
+		//Accessing "value" from the FloatData class
+		moveDirection.Set(MoveX.Value, MoveY.Value, MoveZ.Value);
+		rotDirection.Set(RotX.Value, RotY.Value, RotZ.Value);
+			
+		transform.Rotate(rotDirection);
+			
+		moveDirection = transform.TransformDirection(moveDirection);
+		
+		//moveDirection *= speed; replaced by value in FloatInput
+		if (Input.GetButton("Jump"))
+			moveDirection.y = JumpSpeed.Value;
+
+	}
+
+	moveDirection.y -= Gravity.Value * Time.deltaTime;
+	controller.Move(moveDirection * Time.deltaTime);
+	}
+	
+}
