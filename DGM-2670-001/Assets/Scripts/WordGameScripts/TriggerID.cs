2c4481713721516edffc24297142cf390654e865
Kyle Hinckley <kylehinckley@hotmail.com>
2018-10-01 14:27:39 -0600

    Matching Scripts

---
+++ DGM-2670-001/Assets/Scripts/WordGameScripts/TriggerID.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerID : MonoBehaviour
+{
+
+    public NameID Id;
+    public UnityEvent OnMatch;
+    
+    private void OnTriggerEnter(Collider obj)
+    {
+        if (Id == obj.GetComponent<ObjectID>().Id)
+        {
+            OnMatch.Invoke();
+            print("Match");
+        }
+    }
+    
+}
