533f79732e6f43a223cdfbce7b14e300119dc3f6
Kyle Hinckley <kylehinckley@hotmail.com>
2018-10-10 14:27:19 -0600

    Revert "Drag Fix, Snap Added, need to watch VOD to catch up"
    This reverts commit 48a1c69e28c70db597865e0c2d50302981a916a6.

--- DGM-2670-001/Assets/Scripts/WordGameScripts/Draggable.cs
+++ DGM-2670-001/Assets/Scripts/WordGameScripts/Draggable.cs
@@ -1,35 +1,26 @@
-﻿using System.Collections;
+﻿using Cinemachine.Utility;
 using UnityEngine;
 
 public class Draggable : MonoBehaviour
 {
-	private Vector3 offsetPosition;
+	private Vector3 currentPosition;
 	private Vector3 newPosition;
 	private Camera cam;
-
-
-	public bool CanDrag;
 	
-	private void Start ()
+	void Start ()
 	{
 		cam = Camera.main;
 	}
 
-	public IEnumerator OnMouseDown()
+	private void OnMouseDown()
 	{
-		offsetPosition = transform.position - cam.ScreenToWorldPoint(Input.mousePosition);
-		yield return new WaitForFixedUpdate();
-		CanDrag = true;
-		while (CanDrag)
-		{
-			yield return new WaitForFixedUpdate();
-			newPosition = cam.ScreenToWorldPoint(Input.mousePosition) + offsetPosition;
-			transform.position = newPosition;
-		}
+		currentPosition = transform.position - cam.ScreenToWorldPoint(Input.mousePosition);
 	}
 
-	private void OnMouseUp()
+	private void OnMouseDrag()
 	{
-		CanDrag = false;
+		newPosition = currentPosition + cam.ScreenToWorldPoint(Input.mousePosition);
+		newPosition.z = 0;
+		transform.position = newPosition;
 	}
 }
--- DGM-2670-001/Assets/Scripts/WordGameScripts/MatchID.cs
+++ DGM-2670-001/Assets/Scripts/WordGameScripts/MatchID.cs
@@ -5,14 +5,8 @@ public class MatchID : MonoBehaviour
 {
     public NameID ID;
     public UnityEvent OnMatch;
-
-    private void OnTriggerEnter(Collider other)
-    {
-        var id = other.GetComponent<ObjectID>().ID;
-        Call(id);
-    }
-
-    public void Call(NameID id)
+    
+    public void Invoke (NameID id)
     {
         if (id == ID)
         {
