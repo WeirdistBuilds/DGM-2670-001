6cb39875acbd13035df42d39c535fa6680d18cf2
Kyle Hinckley <kylehinckley@hotmail.com>
2018-10-15 14:38:58 -0600

    Ignore ProCore, Added Store

---
+++ DGM-2670-001/Assets/Scripts/Store/Scripts/Store.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Store", menuName = "Store/Storefront")]
+public class Store : ScriptableObject
+{
+
+	public Objects Available, Purchased;
+	
+	public void MakePurchase(Object obj)
+	{
+		for (var i = 0; i < Available.ObjectList.Count; i++)
+		{
+			var availableObject = Available.ObjectList[i];
+			if (availableObject == obj)
+			{
+				Purchased.ObjectList.Add(obj);
+				Available.ObjectList.Remove(availableObject);
+			}
+		}
+	}
+}
