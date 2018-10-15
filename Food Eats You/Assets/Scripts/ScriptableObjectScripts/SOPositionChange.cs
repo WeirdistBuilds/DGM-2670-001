a477c60199c15b539c24250d56315fc3defdb1ca
Kyle Hinckley <kylehinckley@hotmail.com>
2018-10-14 22:29:30 -0600

    Attempting to create Vector3 SOs

---
+++ Food Eats You/Assets/Scripts/ScriptableObjectScripts/SOPositionChange.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+[CreateAssetMenu(fileName = "NewPositionChange", menuName = "Scriptable Objects/SOPositionChange")]
+public class SOPositionChange : ScriptableObject
+{
+    public SOFloat X, Y, Z;
+    private SOVector3 position;
+    private Vector3 Rotation;
+
+    public void Spawn(GameObject gameObject)
+    {
+        position = new Vector3(X.Value, Y.Value, Z.Value);
+        gameObject.transform.position = position;
+    }
+
+}
---
+++ Food Eats You/Assets/Scripts/ScriptableObjectScripts/SOVector3.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+[CreateAssetMenu(fileName = "NewVector3", menuName = "Scriptable Objects/SOVector3")]
+public class SOVector3 : ScriptableObject
+{
+	public SOFloat X, Y, Z;
+	public Vector3 Vector3
+	{
+		set { new Vector3(X.Value, Y.Value, Z.Value); }
+		get { return Vector3; }
+	}
+}
