4fe9be6a5b0a52bc5f5c2aa6d7be9fe874039491
Kyle Hinckley <kylehinckley@hotmail.com>
2018-10-19 22:50:17 -0600

    Throw Direction Objects

---
+++ Food Eats You/Assets/Scripts/ItemThrow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ItemThrow : ScriptableObject
+{
+    public SOSpawnList ItemSpawnList;
+    public SOInt ThrowDirection;
+    private SOSpawn spawnPoint;
+    
+    public void Invoke()
+    {
+        
+        spawnPoint = ItemSpawnList.SpawnList[Random.Range(0, ItemSpawnList.SpawnList.Count)];
+        
+        //Transform GameObject to that position, movement happens automatically
+        gameObject.transform.position = new Vector3(spawnPoint.EnemyPosition.X.Value, spawnPoint.EnemyPosition.Y.Value, spawnPoint.EnemyPosition.Z.Value);
+        gameObject.transform.Rotate(spawnPoint.EnemyRotation.X.Value, spawnPoint.EnemyRotation.Y.Value, spawnPoint.EnemyRotation.Z.Value);        
+    }
+}
---
+++ Food Eats You/Assets/Scripts/ScriptableObjectScripts/SOInt.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewInt", menuName = "Scriptable Objects/SOInt")]
+public class SOInt : ScriptableObject
+{
+    public int value;
+    public virtual int Value
+    {
+        get { return value; }
+    }
+}
