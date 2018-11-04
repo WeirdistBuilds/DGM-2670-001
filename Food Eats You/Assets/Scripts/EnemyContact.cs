a84a15747cb7de176c1f82420425d5eba34c13c9
Kyle Hinckley <kylehinckley@hotmail.com>
2018-11-03 21:25:15 -0700

    Added Kill Counter

---
+++ Food Eats You/Assets/Scripts/EnemyContact.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyContact : MonoBehaviour
+{
+	public SOInt CurrentKills;
+	
+	private void OnCollisionEnter(Collision other)
+	{
+		Debug.Log("Collision");
+		if (other.transform.CompareTag("Projectile"))
+		{
+			CurrentKills.Value++;
+				Destroy(other.gameObject);
+				Destroy(gameObject);
+			}
+		else if (other.transform.CompareTag("Player"))
+		{
+			CurrentKills.Value = 0;
+			SceneManager.LoadScene("Level01");
+		}
+		else if (other.transform.CompareTag("Super"))
+		{
+			CurrentKills.Value++;
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Food Eats You/Assets/Scripts/TextChange.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TextChange : MonoBehaviour
+{
+	private Text textToChange;
+	public SOInt kills;
+ 
+	void Start () {
+		textToChange = gameObject.GetComponent<Text>(); 
+		textToChange.text = kills.Value.ToString();
+	}
+     
+	void Update () {
+		textToChange.text = kills.Value.ToString(); 
+	}
+}
