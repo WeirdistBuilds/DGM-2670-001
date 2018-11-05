1186d9900454754e75ca130894b55cf24bc781f3
Kyle Hinckley <kylehinckley@hotmail.com>
2018-11-05 15:41:26 -0700

    Modified Purchasable Objects

--- DGM-2670-001/Assets/Scripts/Store/Scripts/PurchasableObject.cs
+++ DGM-2670-001/Assets/Scripts/Store/Scripts/PurchasableObject.cs
@@ -5,4 +5,14 @@ public class PurchasableObject : ScriptableObject
 {
     public int Value;
     public Object Item;
+    public bool Perpetual;
+    public bool Instanciatable;
+
+    public void CreateItem()
+    {
+        if (Instanciatable && Item is GameObject)
+        {
+            Instantiate(Item);
+        }
+    }
 }
